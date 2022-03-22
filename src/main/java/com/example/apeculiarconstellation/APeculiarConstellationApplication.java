71e052b5e2deb68a653f6181bfaef382a91d0915
Joseph Kinahan <kinahanj25@gmail.com>
2022-03-22 22:05:33 +0000

    first commit, boot + angular set up

---
+++ src/main/java/com/example/apeculiarconstellation/APeculiarConstellationApplication.java
@@ -0,0 +1,13 @@
+package com.example.apeculiarconstellation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APeculiarConstellationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APeculiarConstellationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apeculiarconstellation/APeculiarConstellationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apeculiarconstellation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APeculiarConstellationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
